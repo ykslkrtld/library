d4c2d1f0cbc4f5ac4e4d49501709a4b00ba599b5
yksl <yukselkurtuldu9@gmail.com>
2024-07-12 10:13:53 +0300

    fe-react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header style={{textAlign: "center", marginTop:"3rem", color:"purple", fontSize:"1.5rem"}}>
+        <h1>YKSL LIBRARY</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
